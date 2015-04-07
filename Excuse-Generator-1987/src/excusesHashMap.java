8696d226722173b33f484295631fac0fe7ce77a3
Matt Eisenberg <eisenbergm@wit.edu>
2015-04-06 21:15:43 -0400

    Basic bullshit
    -updated excuses list with severity levels, delineated by commas
    -super basic hashmap function stuff, still need to implement file i/o

---
+++ Excuse-Generator-1987/src/excusesHashMap.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Scanner;
+
+public class excusesHashMap {
+
+	HashMap<Integer, String> excuseMap = new HashMap<Integer, String>();
+	Scanner input = new Scanner(System.in);
+	int severity;
+	String excuse;
+}
